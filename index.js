4de94f7cb62768a2394df67a543b03f9ea763159
mcsekar12 <chandrasekar.m12@gmail.com>
2018-12-30 00:10:13 +0530

    socket code

---
+++ index.js
@@ -0,0 +1,51 @@
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+const dotenv = require('dotenv');
+dotenv.config();
+
+const apiai = require('apiai')(process.env.APIAI_CLIENT_TOKEN);
+
+app.get('/send/:id', (req, res) => {
+  console.log(req.params.id);
+  io.sockets
+    .in(req.params.id)
+    .emit('new_msg', { msg: 'hello' + req.params.id });
+  res.send('ok');
+});
+
+io.sockets.on('connection', function(client) {
+  console.log(`Socket ${client.id} connected.`);
+
+  client.on('disconnect', user => {
+    console.log(`Socket ${client.id} disconnected.`);
+  });
+  client.on('authentication', function(user) {
+    client.join(user.userId);
+    console.log(user.userId);
+  });
+
+  client.on('askBot', function(question) {
+    console.log('askBot', question.q);
+    let text = question.q;
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: client.id
+    });
+
+    apiaiReq.on('response', response => {
+      let aiText = response.result.fulfillment.speech;
+      console.log(aiText);
+      client.emit('new_msg', aiText); // Send the result back to the browser!
+    });
+    apiaiReq.on('error', error => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Node started in ${process.env.PORT} port`);
+});
